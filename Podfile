4ca116b3025c5b630b4cdb5e7a8773256e77c6a6
weipeng.w <weipeng.w@macW.local>
2021-01-31 15:48:11 +0800

    pod add

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '10.0'
+
+target 'WSwiftPractice' do
+
+  pod 'YYKit'
+
+end 
+
+
+
+
+
---
+++ Pods/Target Support Files/Pods-WSwiftPractice/Pods-WSwiftPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WSwiftPractice : NSObject
+@end
+@implementation PodsDummy_Pods_WSwiftPractice
+@end
